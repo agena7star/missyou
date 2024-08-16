2dc1c4fe5c8129d8af987ff568cc2dc871b91b3e
agena7star <thebaotran.100604@gmail.com>
2024-08-16 15:05:20 +0700

    2

--- src/controllers/controllers.js
+++ src/controllers/controllers.js
@@ -1,6 +1,16 @@
+import db from "../models/index";
+let getHomePage = async (req, res) => {
+    try {
+        let data = await db.User.findAll();
 
-let getHomePage = (req, res) => {
-    return res.render('homepage.ejs');
+        return res.render('homepage.ejs', {
+            data: JSON.stringify(data)
+        });
+
+    } catch (e) {
+        console.log(e);
+    }
+
 }
 let gethomeview = (req, res) => {
     return res.render('remember/missyou.ejs')
--- src/server.js
+++ src/server.js
@@ -1,7 +1,8 @@
 import express from "express";
 import bodyParser from "body-parser";
 import viewEngine from "./config/viewEngine";
-import initWedRouters from "./route/wed"
+import initWedRouters from "./route/wed";
+import connectDB from "./config/connectDB"
 require("dotenv").config();
 
 let app = express();
@@ -9,6 +10,7 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 viewEngine(app);
 initWedRouters(app);
+connectDB();
 let port = process.env.PORT;
 app.listen(port, () => {
     console.log("backend nodejs is running in the port : " + port);
